9a36a3900cd51904ef8e5d0869eb477b4470479b
David Wogelius <davidwogelius@gmail.com>
2026-05-20 18:24:14 +0200

    initial push
    this is the initial push for my 2026 portfolio

---
+++ src/pages/Index.tsx
@@ -0,0 +1,14 @@
+const Index = () => {
+  return (
+    <>
+        <div className="title-box">
+            <h1>Velkommen til min portfolio</h1>
+        </div>
+        <div className="desc-box">
+            <p>Hej! Jeg er Nuka David, en passioneret softwareudvikler med en forkærlighed for at skabe innovative og brugervenlige digitale løsninger. <br/>Med en solid baggrund inden for både frontend- og backend-udvikling, har jeg erfaring med at arbejde på tværs af hele udviklingsprocessen, fra konceptualisering til implementering.</p>
+        </div>
+    </>
+  )
+}
+
+export default Index
