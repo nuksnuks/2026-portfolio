b4afc9eb106d70f00e6c1bb1010d26e45fa6f8e5
David Wogelius <davidwogelius@gmail.com>
2026-09-18 20:47:49 +0200

    update

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/2026-portfolio">
 
       <App />
     </BrowserRouter>
